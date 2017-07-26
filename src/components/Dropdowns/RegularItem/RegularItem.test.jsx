4ffe5297be9469bf71108a8a621c8d252040b281
Aliaksei Martsinkevich <martin.ljesha@gmail.com>
2017-07-26 15:34:41 +0300

    Add remark test for regular item

--- src/components/Dropdowns/RegularItem/RegularItem.test.jsx
+++ src/components/Dropdowns/RegularItem/RegularItem.test.jsx
@@ -4,7 +4,7 @@ import RegularItem from './RegularItem';
 import icon from '../../../../assets/icons/svg/collapse_arrow-black.svg';
 
 describe('Regular item', () => {
-  it('Should render without fails', () => {
+  it('Should render with icon', () => {
     const customClassName = 'class-name-example';
     const text = 'Some text';
     const onClick = jest.fn();
@@ -13,6 +13,21 @@ describe('Regular item', () => {
         className={customClassName}
         text={text}
         iconUri={icon}
+        onClick={onClick}
+      />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Should render with remark', () => {
+    const customClassName = 'class-name-example';
+    const text = 'Some text';
+    const onClick = jest.fn();
+    const component = renderer.create(
+      <RegularItem
+        className={customClassName}
+        text={text}
+        remark={text}
         onClick={onClick}
       />);
     const tree = component.toJSON();
